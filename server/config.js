a4380d2400d152028626f52a7cb8a61a1e25f797
chrisChatzi <christos.chatziioannidis@gmail.com>
2018-08-11 16:19:04 +0200

    mongo solution

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 var port = 1924;
-var mongoPath = "mongodb://dvdent:dv-service-HM74-m@ds219532.mlab.com:19532/dvdent";
+var mongoPath = "mongodb://christos:dv-service-HM74-m@ds219532.mlab.com:19532/dvdent";
 
 module.exports = {
 	port: port,
